175aca70423b93b1c4f8a4b7cf7ae5148384f922
tanaka <tanaka.mawoyo@yahoo.com>
2022-06-04 19:56:04 +0200

    first commit

---
+++ NBA/TeamsInfo.swift
@@ -0,0 +1,42 @@
+//
+//  TeamsInfo.swift
+//  NBA
+//
+//  Created by Tanaka Mawoyo on 04.06.22.
+//
+
+import Foundation
+import UIKit
+
+struct TeamsInfo {
+    let logo: UIImage
+    let name: String
+    let site: String
+}
+
+extension TeamsInfo {
+    
+    static var all: [TeamsInfo] = [
+        TeamsInfo(logo: UIImage(named: "Atlanta-Hawks")!, name: "Atlanta Hawks", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: ""),
+        TeamsInfo(logo: UIImage(named: "")!, name: "", site: "")
+      
+    ]
+
+}
---
+++ NBA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NBA
+//
+//  Created by Tanaka Mawoyo on 04.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
